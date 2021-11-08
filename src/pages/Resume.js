c72c7c64955a2505966509a4c5c27946739200cf
Sean <sdillon215@gmail.com>
2021-11-08 00:01:06 -0800

    resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,10 +2,47 @@ import React from 'react';
 
 const Resume = () => {
     return (
-        <div>
-            Resume
-        </div>
-    );
+        <section className="section top">
+            
+            <div className="stuff">
+                <br></br>
+                    <h1 className="title">Resume Placeholder</h1>
+                    <hr />
+                        <p className="title">Interests</p>
+                        <ul className="resume-list">
+                            <li>Drawing</li>
+                            <li>Photography</li>
+                            <li>Design</li>
+                            <li>Programming</li>
+                            <li>Computer Science</li>
+                        </ul>
+                        <p className="title">Skills</p>
+                        <ul className="resume-list">
+                            <li>Web Design with HTML & CSS</li>
+                        </ul>
+                        <p className="title">Education</p>
+                        <ul className="resume-list">
+                            <a href="http://www.wiltonhighschool.org/pages/Wilton_High_School">
+                                <li>Wilton High School</li>
+                            </a>
+                            <a href="https://www.silvermineart.org/">
+                                <li>Silvermine School of Arts</li>
+                            </a>
+                        </ul>
+                        <p className="title">Experience</p>
+                        <ul className="resume-list">
+                            <li>Student Technology Intern for Wilton School District</li>
+                            <li>Babysitter</li>
+                        </ul>
+                        <p className="title">Extracurriculars</p>
+                        <ul className="resume-list">
+                            <li>Recycling Club</li>
+                            <li>Gardening Club</li>
+                            <li>Book Club</li>
+                        </ul>
+                    </div>
+                </section>
+                    );
 };
 
 export default Resume;
