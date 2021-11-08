2a7f4aa7f9044357a3cbaa089409b4062b37f8f3
Sean <sdillon215@gmail.com>
2021-11-07 21:06:18 -0800

    nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 const Header = () => {
 
-
   return (
-    <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-          <h1>Sean Portfolio</h1>
-        </Link>
+    <header>
+      <Link className="header-link" to="/">
+        <h1>Sean Dillon</h1>
+      </Link>
 
-        <nav className="text-center">
-            <>
-              <Link to="/">About</Link>
-              <Link to="/Portfolio">Portfolio</Link>
-              <Link to="/Contact">Contact</Link>
-              <Link to="/Resume">Resume</Link>
-
-              
-            </>
-        </nav>
-      </div>
+      <ul>
+        <li>
+          <NavLink exact className="header-link tab" activeClassName="active" to="/">About</NavLink>
+        </li>
+        <li>
+          <NavLink className="header-link tab" activeClassName="active" to="/Portfolio">Portfolio</NavLink>
+        </li>
+        <li>
+          <NavLink className="header-link tab" activeClassName="active" to="/Contact">Contact</NavLink>
+        </li>
+        <li>
+          <NavLink className="header-link tab" activeClassName="active" to="/Resume">Resume</NavLink>
+        </li>
+      </ul>
     </header>
   );
 };
